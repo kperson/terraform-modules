e17a048fecf876437564303f49e75e24b5d6babf
Kelton Person <keltonperson@Keltons-MacBook-Pro.local>
2019-07-23 19:10:59 -0500

    added dynamo crud policy

---
+++ dynamo-crud-policy/main.tf
@@ -0,0 +1,58 @@
+variable "table_arn" {
+  type = "string"
+}
+
+variable "stream_arn" {
+  type    = "string"
+  default = null
+}
+
+
+data "aws_iam_policy_document" "document" {
+  dynamic "statement" {
+    for_each = var.stream_arn == null ? [] : list(var.stream_arn)
+    actions = [
+      "dynamodb:DescribeStream",
+      "dynamodb:GetRecords",
+      "dynamodb:GetShardIterator",
+    ]
+
+    resources = [
+      "${attribute.value}"
+    ]
+
+  }
+
+  statement {
+    actions = [
+      "dynamodb:ListStreams",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:BatchWriteItem",
+      "dynamodb:BatchGetItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:Scan",
+      "dynamodb:Query",
+      "dynamodb:UpdateItem",
+    ]
+
+    resources = [
+      "${var.table_arn}"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "policy" {
+  policy = "${data.aws_iam_policy_document.document.json}"
+}
+
+output "policy_arn" {
+  value = "${aws_iam_policy.policy.arn}"
+}
