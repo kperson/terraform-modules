5c1d1dad1f137d0c4d8e0241e65ef80895c6a4f5
Kelton Person <kelton.person@gmail.com>
2022-02-06 10:46:51 -0600

    updated synxtax to new terraforma

--- auto-scaled-dynamo/main.tf
+++ auto-scaled-dynamo/main.tf
@@ -32,19 +32,19 @@ variable "point_in_time_recovery_enabled" {
 }
 
 variable "global_secondary_indices" {
-  type = [object({
+  type = tolist([object({
     name      = string
     hash_key  = string
     range_key = string
-  })]
+  })])
   default = []
 }
 
 variable "local_secondary_indices" {
-  type = [object({
+  type = tolist([object({
     name      = string
     range_key = string
-  })]
+  })])
   default = []
 }
 
